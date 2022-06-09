0e8cd75cee31d1edc3a79ac14f253045fcec216a
Putin01001010010101010 <rusfastik483@gmail.com>
2022-06-09 21:27:05 +0300

    new start

--- src/app/shared/service/weather.service.ts
+++ src/app/shared/service/weather.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?`;
+const API_KEY = ``
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
